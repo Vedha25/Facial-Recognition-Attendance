0d1651b7c10db3fae332be1667ff1eb835fbc1a2
Vedha25 <vethabalasamuel04@gmail.com>
2025-03-13 20:23:47 +0530

    Update FRA.py

--- FRA.py
+++ FRA.py
@@ -30,8 +30,7 @@ while True:
     _, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-
-    #RECOGNIZE FACES
+    
     face_locations = face_recognition.face_locations(rgb_small_frame)
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
